1370581313c55b4c4748b2e7e5ed48994a069059
dgyrsenevirathne <yasithsenevirathna01@gmail.com>
2024-03-15 21:47:45 +0530

    line 52 name change

--- lib/pages/validationFlow/login_page.dart
+++ lib/pages/validationFlow/login_page.dart
@@ -49,7 +49,7 @@ class _LoginPageState extends State<LoginPage> {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       const Text(
-                        'Welcome Yasith',
+                        'Welcome sonal',
                         style: TextStyle(
                           fontSize: 30,
                           color: CustomColor.textBlack,
