795070414fd92bfdbf97bd845bb8cc6a74038869
Shashibotheju <shashiupendra100@gmail.com>
2024-03-16 23:10:02 +0530

    edit welcome back

--- lib/pages/validationFlow/login_page.dart
+++ lib/pages/validationFlow/login_page.dart
@@ -51,7 +51,7 @@ class _LoginPageState extends State<LoginPage> {
                       mainAxisAlignment: MainAxisAlignment.start,
                       children: [
                         const Text(
-                          'Welcome shashi',
+                          'Welcome back',
                           style: TextStyle(
                             fontSize: 30,
                             color: CustomColor.textBlack,
