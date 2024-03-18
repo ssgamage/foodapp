9850b5bfad8c4ced089094178c9c04451fcc841f
Thamara Deeptha <Thamaradeepthanew@gmail.com>
2024-03-18 15:23:14 +0530

    colour change

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,10 +1,24 @@
 import 'dart:ui';
 
 class CustomColor {
-  static const Color backgroundMain = Color.fromARGB(255, 231, 221, 201);
-  static const Color backgroundSecondary = Color.fromARGB(255, 230, 170, 102);
-  static const Color orangeMain = Color.fromARGB(255, 234, 122, 10);
-  static const Color warningFieldBg = Color.fromARGB(255, 238, 232, 75);
+  static const Color backgroundMain =
+      Color.fromARGB(255, 231, 221, 201); //EEE9E9
+  static const Color backgroundSecondary =
+      Color.fromARGB(255, 230, 170, 102); //F4E9C8
+  static const Color orangeMain = Color.fromARGB(255, 234, 122, 10); //E27602
+  static const Color warningFieldBg =
+      Color.fromARGB(255, 238, 232, 75); //FFAA2C
   static const Color textBlack = Color.fromARGB(255, 0, 0, 0);
   static const Color textWhite = Color.fromARGB(255, 255, 255, 255);
+
+  //FFC107
+
+  //loging page button linear code 1-  E89204  (232 . 144. 4)
+  //login page button linear code 2 -E89204  (236 . 144. 6)
+
+  //Lh11   -FD5C2E (353. 92.46)
+  //Lh12   -FE7122 (254.113.34)
+
+  //l1  - E89204 ()
+  //l2  -EC9006  ()
 }
